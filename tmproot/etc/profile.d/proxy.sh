c1a51c0340bf3aceb9fde3eefe9bc189a63f0794
Natanael Copa <ncopa@alpinelinux.org>
2014-05-30 15:48:47 +0200

    ==== release 3.0.2 ====

---
+++ tmproot/etc/profile.d/proxy.sh
@@ -0,0 +1,5 @@
+# this file was generated with and might get overwritten by setup-proxy
+
+export http_proxy=http://proxy.nor.wtbts.net:8080
+export https_proxy=http://proxy.nor.wtbts.net:8080
+export ftp_proxy=http://proxy.nor.wtbts.net:8080
